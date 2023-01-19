553fb494215f8138a7587561d0cbebf60db25a52
rahulsya <rahulsyaban@gmail.com>
2023-01-19 23:30:22 +0700

    feat(env):add config env

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,17 +12,19 @@ import { entities } from './utils/entity';
 
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { HotelsModule } from './hotels/hotels.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     UsersModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'qwerty12',
-      database: 'staycation',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities,
       synchronize: true,
     }),
--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,3 +1,3 @@
 export const jwtConstants = {
-  secret: 'sangatRahasia',
+  secret: process.env.JWT_SECRET,
 };
